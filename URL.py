3435d615785924826c2d5b9e12ef3fac07687311
A.Ramya <71496573+Ramya-Adepu@users.noreply.github.com>
2021-07-14 09:10:19 +0530

    Add files via upload
    url cache problem

---
+++ URL.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+ 
+class LRUCache:
+    def _init_(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+ 
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            print("-1")
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+ 
+ 
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+
+cache = LRUCache(3) 
+
+cache.put(1, 1)
+print(cache.cache)
+cache.put(2, 2)
+print(cache.cache)
+cache.put(3, 3)
+print(cache.cache)
+cache.get(2)
+print(cache.cache)
+cache.get(3)
+print(cache.cache)
+cache.put(5,5)
+print(cache.cache)
+cache.put(6,6)
+print(cache.cache)
+cache.get(6)
+print(cache.cache)
+cache.get(4)
+print(cache.cache)
